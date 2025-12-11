7dd5284a10e07e8869e483ddb539f0e90fc2ced4
Pompeu Costa <pompeugcosta@gmail.com>
2025-12-11 16:11:04 +0000

    index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -104,23 +104,33 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f,-0.5f, 
-         0.0f, 0.5f, 
-         0.5f,-0.5f,
+         0.5f,-0.5f, 
+         0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] = {
+        0,1,2,
+        2,3,0
     };
 
     unsigned int buffer;
     glGenBuffers(1,&buffer);
     glBindBuffer(GL_ARRAY_BUFFER,buffer);
-    glBufferData(GL_ARRAY_BUFFER,6 * sizeof(float),positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER,8 * sizeof(float),positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2 * sizeof(float),0);
 
+    unsigned int ibo;
+    glGenBuffers(1,&ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,6 * sizeof(unsigned int),indices, GL_STATIC_DRAW);
+
+
     ShaderProgramSource source = parseShader("../src/res/shaders/Basic.shader");
-    std::cout << "Vertex\n" << source.VertexSource << std::endl;
-    std::cout << "Fragment\n"<< source.FragmentSource << std::endl;
     
     unsigned int shader = createShader(source.VertexSource,source.FragmentSource);
     glUseProgram(shader);
@@ -131,7 +141,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES,0, 3);
+        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
