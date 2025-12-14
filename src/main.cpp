24ccf93730fb55bec332f54afadfd0ea66f7ecca
Pompeu Costa <pompeugcosta@gmail.com>
2025-12-14 12:10:12 +0000

    add uniform buffer for color

--- src/main.cpp
+++ src/main.cpp
@@ -137,6 +137,7 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     GLenum err = glewInit();
     // if(err != GLEW_OK) {
@@ -180,13 +181,30 @@ int main(void)
     unsigned int shader = createShader(source.VertexSource,source.FragmentSource);
     glUseProgram(shader);
 
+    int location = glGetUniformLocation(shader,"u_Color");
+    if(location == -1) {
+        std::cerr << "Uniform location cant be found" << std::endl;
+        return 1;
+    }
+
+    float r = 0.8f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUniform4f(location,r,0.3f,0.8f,1.0f);
         glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,nullptr);
+
+        if(r > 1.0f)
+            r = -0.05f;
+        else if(r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
